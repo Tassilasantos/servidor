583a83c09da3467898427546d76bdc43ec8cfc3f
Tássila Santos <82525851+Tassilasantos@users.noreply.github.com>
2021-08-04 14:14:40 -0300

    Update server.js

--- server/server.js
+++ server/server.js
@@ -1,6 +1,6 @@
-const dados = require("./data/ghibli.json") // -> definição de uma variavel para um dado especifico 
-const express = require ("express") // -> chama o express
-const server = express()            // -> define a variavel do express
+const dados = require("./data/ghibli.json") 
+const express = require ("express")
+const server = express()            
 
 server.listen(1313, ()=>{
     console.log("Inicializador!")
@@ -12,11 +12,11 @@ server.get("/", (request, response) =>{
 })
 server.get("/dados", (request, response)=>{
 
-    response.status(200).send(dados) // definindo um comando para que um dado especifico seja mostrado(dados)
+    response.status(200).send(dados) 
 })
 
 server.get("/dados/:id", (request, response)=>{
-    const idRequest = request.params.id // recolhendo o parametro enviado na request
+    const idRequest = request.params.id 
 
     let dadosFiltrados = dados.find(dado => dado.id == idRequest)
     console.log(dadosFiltrados)
@@ -25,7 +25,7 @@ server.get("/dados/:id", (request, response)=>{
             mensagem: "ID inexistente!"
         })
     }else{
-        response.status(201).send(dadosFiltrados) // mostra a respota no postman
+        response.status(201).send(dadosFiltrados) 
     }
 
 })
